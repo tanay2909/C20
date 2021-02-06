903359232d08eac8a0fc339e36f529e7e4c7030c
tanay2909 <76608462+tanay2909@users.noreply.github.com>
2021-02-06 11:59:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+var fixedrect
+var movingrect
+function setup() {
+  createCanvas(800,400);
+ 
+ fixedrect =  createSprite(400, 200, 50, 50);
+movingrect = createSprite(400,200,90,70)
+
+}
+ 
+function draw() {
+
+  background(255,255,255);  
+
+  movingrect.x=mouseX
+  movingrect.y= mouseY
+  
+  if(movingrect.x - fixedrect.x <= movingrect.width/2 + fixedrect.width/2 
+    &&  fixedrect.x - movingrect.x <= movingrect.width/2 + fixedrect.width/2 
+    && movingrect.y - fixedrect.y <= movingrect.height/2 + fixedrect.height/2 
+    &&  fixedrect.y - movingrect.y <= movingrect.height/2 + fixedrect.height/2  )
+  { 
+    fixedrect.shapeColor="red"
+    movingrect.shapeColor="red"
+
+  }
+  else {
+    fixedrect.shapeColor="green"
+    movingrect.shapeColor="green"
+ 
+  }
+
+  drawSprites();
+}
